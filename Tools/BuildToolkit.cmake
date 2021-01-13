b2d1b302711c7d4a6da82adfc6089f9033927709
Derek Nylen <dwnylen@hurlybird.com>
2021-01-13 09:35:22 -0800

    More reliable CMake file/header copying

--- Tools/BuildToolkit.cmake
+++ Tools/BuildToolkit.cmake
@@ -27,63 +27,54 @@ endfunction()
 
 # Copy Targets ===========================================================================
 
-# Create a copy target
-function( add_copy_target TargetName DestinationDir )
-    add_custom_target( ${TargetName} )
-    set_target_properties( ${TargetName} PROPERTIES "COPY_DESTINATION" ${DestinationDir} )
+# Copy files to a destination
+function( copy_files TargetName DestinationDir )
+    set( DstFiles "" )
 
-    add_custom_command( TARGET ${TargetName} PRE_BUILD
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${DestinationDir} )
-endfunction()
-
-
-# Add files to a copy target
-function( target_copy_files TargetName )
-    get_target_property( DestinationDir ${TargetName} "COPY_DESTINATION" )
-    foreach( argi ${ARGN} )
-        add_custom_command( TARGET ${TargetName} PRE_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${argi} ${DestinationDir} )
+    foreach( srci ${ARGN} )
+        get_filename_component( filename ${srci} NAME )
+        set( dsti ${DestinationDir}/${filename} )
+        set( DstFiles ${DstFiles} ${dsti} )
+        add_custom_command(
+            OUTPUT ${dsti}
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${srci} ${DestinationDir}
+            MAIN_DEPENDENCY ${srci}
+            COMMENT "Copying ${filename}" )
     endforeach()
+
+    add_custom_target( ${TargetName} ALL
+        #COMMENT "Copying files to ${DestinationDir}..."
+        DEPENDS ${DstFiles} )
+
+    add_custom_command( TARGET ${TargetName}
+        PRE_BUILD COMMAND ${CMAKE_COMMAND} -E make_directory ${DestinationDir} )
 endfunction()
 
 
-# Add header files to a copy target
-function( target_copy_headers TargetName )
-    get_target_property( DestinationDir ${TargetName} "COPY_DESTINATION" )
-    foreach( argi ${ARGN} )
-        if( ${argi} MATCHES "^.*\\.(h)$" )
-            add_custom_command( TARGET ${TargetName} PRE_BUILD
-                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${argi} ${DestinationDir} )
+# Copy header files to a destination
+function( copy_header_files TargetName DestinationDir )
+    set( DstFiles "" )
+
+    foreach( srci ${ARGN} )
+        if( ${srci} MATCHES "^.*\\.(h)$" )
+            get_filename_component( filename ${srci} NAME )
+            set( dsti ${DestinationDir}/${filename} )
+            set( DstFiles ${DstFiles} ${dsti} )
+            add_custom_command(
+                OUTPUT ${dsti}
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${srci} ${DestinationDir}
+                MAIN_DEPENDENCY ${srci}
+                COMMENT "Copying ${filename}" )
         endif()
     endforeach()
+
+    add_custom_target( ${TargetName} ALL
+        #COMMENT "Copying header files to ${DestinationDir}..."
+        DEPENDS ${DstFiles} )
+
+    add_custom_command( TARGET ${TargetName}
+        PRE_BUILD COMMAND ${CMAKE_COMMAND} -E make_directory ${DestinationDir} )
 endfunction()
-
-
-# Add directories to a copy target
-function( target_copy_directories TargetName )
-    get_target_property( DestinationDir ${TargetName} "COPY_DESTINATION" )
-    foreach( argi ${ARGN} )
-        add_custom_command( TARGET ${TargetName} PRE_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_directory ${argi} ${DestinationDir} )
-    endforeach()
-endfunction()
-
-
-# Add files matching a glob pattern to a copy target
-function( target_copy_matching_in_directories TargetName Patterns )
-    get_target_property( DestinationDir ${TargetName} "COPY_DESTINATION" )
-    foreach( argi ${ARGN} )
-        foreach( Pattern ${Patterns} )
-            file( GLOB Files ${argi}/${Pattern} )
-
-            foreach( File ${Files} )
-                add_custom_command( TARGET ${TargetName} PRE_BUILD
-                    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${File} ${DestinationDir} )
-            endforeach()
-        endforeach()
-    endforeach()
-endfunction()
-
 
 
 # Target Headers/Sources =================================================================
